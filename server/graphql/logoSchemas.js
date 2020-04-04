0c6960ea6b22ac7d86a87f55683446179240da8f
tyuen11 <timothy.yuen11@gmail.com>
2020-04-04 00:15:42 -0400

    added more attributes to logo schema

--- server/graphql/logoSchemas.js
+++ server/graphql/logoSchemas.js
@@ -27,6 +27,24 @@ var logoType = new GraphQLObjectType({
             },
             lastUpdate: {
                 type: GraphQLDate
+            },
+            backgroundColor: {
+                type: GraphQLString
+            },
+            borderColor: {
+                type: GraphQLString
+            },
+            borderRadius: {
+                type: GraphQLInt
+            },
+            borderWidth: {
+                type: GraphQLInt
+            },
+            padding: {
+                type: GraphQLInt
+            },
+            margin: {
+                type: GraphQLInt
             }
         }
     }
@@ -83,22 +101,22 @@ var mutation = new GraphQLObjectType({
                         type: new GraphQLNonNull(GraphQLInt)
                     },
                     backgroundColor: {
-                        type = new GraphQLNonNull(GraphQLString)
+                        type: new GraphQLNonNull(GraphQLString)
                     },
                     borderColor: {
-                        type = new GraphQLNonNull(GraphQLString)
+                        type: new GraphQLNonNull(GraphQLString)
                     },
                     borderRadius: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     },
                     borderWidth: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     },
                     padding: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     },
                     margin: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     }
                 },
                 resolve: function (root, params) {
@@ -127,26 +145,27 @@ var mutation = new GraphQLObjectType({
                         type: new GraphQLNonNull(GraphQLInt)
                     },
                     backgroundColor: {
-                        type = new GraphQLNonNull(GraphQLString)
+                        type: new GraphQLNonNull(GraphQLString)
                     },
                     borderColor: {
-                        type = new GraphQLNonNull(GraphQLString)
+                        type: new GraphQLNonNull(GraphQLString)
                     },
                     borderRadius: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     },
                     borderWidth: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     },
                     padding: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     },
                     margin: {
-                        type = new GraphQLNonNull(GraphQLInt)
+                        type: new GraphQLNonNull(GraphQLInt)
                     }
                 },
                 resolve(root, params) {
-                    return LogoModel.findByIdAndUpdate(params.id, { text: params.text, color: params.color, fontSize: params.fontSize, lastUpdate: new Date() }, function (err) {
+                    return LogoModel.findByIdAndUpdate(params.id, { text: params.text, color: params.color, fontSize: params.fontSize, lastUpdate: new Date(), backgroundColor: params.backgroundColor,
+                    borderColor: params.borderColor, borderRadius: params.borderRadius, borderWidth: params.borderWidth, padding: params.padding, margin: params.margin }, function (err) {
                         if (err) return next(err);
                     });
                 }
--- server/models/Logo.js
+++ server/models/Logo.js
@@ -6,6 +6,12 @@ var LogoSchema = new mongoose.Schema({
   color: String,
   fontSize: { type: Number, min: 2, max: 144 },
   lastUpdate: { type: Date, default: Date.now },
+  backgroundColor: String,
+  borderColor: String, 
+  borderRadius: { type: Number, min: 2, max: 144 },
+  borderWidth: { type: Number, min: 2, max: 144 },
+  padding: { type: Number, min: 2, max: 144 },
+  margin: { type: Number, min: 2, max: 144 }
 });
 
 module.exports = mongoose.model('Logo', LogoSchema);
