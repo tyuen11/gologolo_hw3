9eb645e053f66bc21d7dd3e91ccb4bfff257417e
tyuen11 <timothy.yuen11@gmail.com>
2020-04-05 02:17:12 -0400

    display the logo

--- client/src/components/ViewLogoScreen.js
+++ client/src/components/ViewLogoScreen.js
@@ -4,6 +4,7 @@ import '../App.css';
 import gql from 'graphql-tag';
 import { Query, Mutation } from 'react-apollo';
 import Navbar from './editScreen/Navbar.js'
+import ViewLogo from './editScreen/ViewLogo';
 
 
 const GET_LOGO = gql`
@@ -14,6 +15,12 @@ const GET_LOGO = gql`
             color
             fontSize
             lastUpdate
+            backgroundColor
+            borderColor
+            borderRadius
+            borderWidth
+            padding
+            margin
         }
     }
 `;
@@ -35,42 +42,37 @@ class ViewLogoScreen extends Component {
                     if (loading) return 'Loading...';
                     if (error) return `Error! ${error.message}`;
 
+                    console.log(data.logo);
+                    
+
                     return (
                         <div className="container">
                             <div className="panel panel-default">
                                 <div className="panel-heading">
                                     <Navbar></Navbar>
-                                    <h3 className="panel-title">
-                                        View Logo
-                                    </h3>
                                 </div>
-                                <div className="panel-body">
-                                    <dl>
-                                        <dt>Text:</dt>
-                                        <dd>{data.logo.text}</dd>
-                                        <dt>Color:</dt>
-                                        <dd>{data.logo.color}</dd>
-                                        <dt>Font Size:</dt>
-                                        <dd>{data.logo.fontSize}</dd>
-                                        <dt>Last Updated:</dt>
-                                        <dd>{data.logo.lastUpdate}</dd>
-                                    </dl>
-                                    <Mutation mutation={DELETE_LOGO} key={data.logo._id} onCompleted={() => this.props.history.push('/')}>
-                                        {(removeLogo, { loading, error }) => (
-                                            <div>
-                                                <form
-                                                    onSubmit={e => {
-                                                        e.preventDefault();
-                                                        removeLogo({ variables: { id: data.logo._id } });
-                                                    }}>
-                                                    <Link to={`/edit/${data.logo._id}`} className="btn btn-success">Edit</Link>&nbsp;
-                                                <button type="submit" className="btn btn-danger">Delete</button>
-                                                </form>
-                                                {loading && <p>Loading...</p>}
-                                                {error && <p>Error :( Please try again</p>}
-                                            </div>
-                                        )}
-                                    </Mutation>
+                                <div className="row">
+                                    <div className="col-sm-12"> 
+                                        <ViewLogo logo={data.logo}/>
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <Mutation mutation={DELETE_LOGO} key={data.logo._id} onCompleted={() => this.props.history.push('/')}>
+                                            {(removeLogo, { loading, error }) => (
+                                                <div>
+                                                    <form
+                                                        onSubmit={e => {
+                                                            e.preventDefault();
+                                                            removeLogo({ variables: { id: data.logo._id } });
+                                                        }}>
+                                                        <Link to={`/edit/${data.logo._id}`} className="btn btn-success">Edit</Link>&nbsp;
+                                                    <button type="submit" className="btn btn-danger">Delete</button>
+                                                    </form>
+                                                    {loading && <p>Loading...</p>}
+                                                    {error && <p>Error :( Please try again</p>}
+                                                </div>
+                                            )}
+                                        </Mutation>
+                                    </div>
                                 </div>
                             </div>
                         </div>
---
+++ client/src/components/editScreen/ViewLogo.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class ViewLogo extends Component {
+    render() {
+        const styles = {
+            container: {
+               
+                color: this.props.logo.textColor,
+                fontSize: this.props.logo.fontSize + "pt",
+                backgroundColor: this.props.logo.backgroundColor,
+                borderRadius: this.props.logo.borderRadius + "pt",
+                padding: this.props.logo.padding + "pt",
+                margin: this.props.logo.margin + "pt",
+                position: "absolute",
+                border: this.props.logo.borderColor + " solid " + this.props.logo.borderWidth + "pt",
+
+            }
+        }
+        return (
+            <div>
+                <div style={ styles.container }>
+                    {this.props.logo.text}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ViewLogo
